6f180284c2333ef29da9939d184bb341034605f9
Jeniquef <jeniquefahie@gmail.com>
2025-01-31 18:25:32 -0500

    Update api_keys.py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "54ca040275188697d8dc7bf493d7c5ba"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "3df18988cf9b40b0a606dae7790f62ec"
